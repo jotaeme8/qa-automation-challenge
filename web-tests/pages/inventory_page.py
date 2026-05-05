f8ffa73c8c111669c9b89dbd07130c5f1dc55224
jotaeme8 <jmbritodearaujo@gmai.com>
2026-05-05 07:37:53 -0300

    fix: aguarda cart.html após clicar no carrinho

--- web-tests/pages/inventory_page.py
+++ web-tests/pages/inventory_page.py
@@ -1,31 +1,25 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
+PRODUCT_BUTTON_IDS = {
+    "Sauce Labs Backpack": "add-to-cart-sauce-labs-backpack",
+    "Sauce Labs Bike Light": "add-to-cart-sauce-labs-bike-light",
+}
 
 class InventoryPage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 15)
+        self.wait = WebDriverWait(driver, 30)
 
     def is_loaded(self):
         self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "inventory_list")))
         return True
 
     def add_item_to_cart(self, item_name):
-        self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "inventory_item")))
-        items = self.driver.find_elements(By.CLASS_NAME, "inventory_item")
-        for item in items:
-            name = item.find_element(By.CLASS_NAME, "inventory_item_name").text
-            if item_name in name:
-                btn = item.find_element(By.TAG_NAME, "button")
-                self.wait.until(EC.element_to_be_clickable(btn))
-                self.driver.execute_script("arguments[0].scrollIntoView(true);", btn)
-                time.sleep(0.5)
-                self.driver.execute_script("arguments[0].click();", btn)
-                time.sleep(0.5)
-                return
-        raise ValueError(f"Produto nao encontrado: {item_name}")
+        button_id = PRODUCT_BUTTON_IDS.get(item_name)
+        btn = self.wait.until(EC.element_to_be_clickable((By.ID, button_id)))
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def get_cart_count(self):
         try:
@@ -35,4 +29,6 @@ class InventoryPage:
             return 0
 
     def go_to_cart(self):
-        self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        link = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link")))
+        self.driver.execute_script("arguments[0].click();", link)
+        self.wait.until(EC.url_contains("cart.html"))
