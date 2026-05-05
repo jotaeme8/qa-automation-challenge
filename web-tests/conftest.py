dc8839bb2ff91ed69688d76a144905b5578162c1
jotaeme8 <jmbritodearaujo@gmai.com>
2026-05-05 07:20:32 -0300

    fix: aumenta timeouts e melhora estabilidade no CI

--- web-tests/conftest.py
+++ web-tests/conftest.py
@@ -9,6 +9,11 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
     chrome = webdriver.Chrome(options=options)
+    chrome.set_page_load_timeout(60)
+    chrome.implicitly_wait(10)
     yield chrome
     chrome.quit()
--- web-tests/pages/checkout_page.py
+++ web-tests/pages/checkout_page.py
@@ -5,20 +5,23 @@ from selenium.webdriver.support import expected_conditions as EC
 class CheckoutPage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 30)
 
     def proceed_to_checkout(self):
-        self.wait.until(EC.element_to_be_clickable((By.ID, "checkout"))).click()
+        btn = self.wait.until(EC.element_to_be_clickable((By.ID, "checkout")))
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def fill_form(self, first_name, last_name, zip_code):
         self.wait.until(EC.presence_of_element_located((By.ID, "first-name")))
         self.driver.find_element(By.ID, "first-name").send_keys(first_name)
         self.driver.find_element(By.ID, "last-name").send_keys(last_name)
         self.driver.find_element(By.ID, "postal-code").send_keys(zip_code)
-        self.driver.find_element(By.ID, "continue").click()
+        btn = self.wait.until(EC.element_to_be_clickable((By.ID, "continue")))
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def finish_order(self):
-        self.wait.until(EC.element_to_be_clickable((By.ID, "finish"))).click()
+        btn = self.wait.until(EC.element_to_be_clickable((By.ID, "finish")))
+        self.driver.execute_script("arguments[0].click();", btn)
 
     def get_confirmation_text(self):
         element = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "complete-header")))
